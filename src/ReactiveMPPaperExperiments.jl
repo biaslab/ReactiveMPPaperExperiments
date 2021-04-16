3b89e8e330e4229fe5d23aaa8a17841a21af0dc8
Bagaev Dmitry <bvdmitri@gmail.com>
2021-04-16 17:33:57 +0200

    Update folder structure with DrWatson.jl package

--- src/ReactiveMPPaperExperiments.jl
+++ src/ReactiveMPPaperExperiments.jl
@@ -1,35 +1,35 @@
 module ReactiveMPPaperExperiments
 
-include("helpers/throttled_slider.jl")
+# include("helpers/throttled_slider.jl")
 
-using Plots
-using PGFPlotsX
+# using Plots
+# using PGFPlotsX
 
-is_in_pluto() = in(:PlutoRunner, names(Main))
+# is_in_pluto() = in(:PlutoRunner, names(Main))
 
-function instantiate()
-    if is_in_pluto()
-        gr()
-    else
-        eval("using PGFPlotsX")
-        pgfplotsx()
-        println("Script instantiated")
-    end
+# function instantiate()
+#     if is_in_pluto()
+#         gr()
+#     else
+#         eval("using PGFPlotsX")
+#         pgfplotsx()
+#         println("Script instantiated")
+#     end
 
-    return nothing
-end
+#     return nothing
+# end
 
-function saveplot(p, name)
-    if is_in_pluto()
-        return p
-    end
+# function saveplot(p, name)
+#     if is_in_pluto()
+#         return p
+#     end
 
-    savefig(p, "figures/$(name).tikz")
-    savefig(p, "figures/$(name).png")
+#     savefig(p, "figures/$(name).tikz")
+#     savefig(p, "figures/$(name).png")
 
-    println("Saved figures/$(name)*")
+#     println("Saved figures/$(name)*")
     
-    return p
-end
+#     return p
+# end
 
 end # module
