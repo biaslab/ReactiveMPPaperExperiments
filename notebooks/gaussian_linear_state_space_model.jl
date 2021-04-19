edad0f3d39ae320711dbaa14cb627c480e493ba9
Bagaev Dmitry <bvdmitri@gmail.com>
2021-04-19 16:19:42 +0200

    update(): Update linear gaussian state space model experiment with benchmarks

--- notebooks/gaussian_linear_state_space_model.jl
+++ notebooks/gaussian_linear_state_space_model.jl
@@ -29,6 +29,7 @@ end
 begin
 	using PlutoUI, Images
     using ReactiveMP, Rocket, GraphPPL, Distributions, Random, Plots
+	using BenchmarkTools, DataFrames
 	if !in(:PlutoRunner, names(Main))
 		using PGFPlotsX
 		pgfplotsx()
@@ -129,22 +130,17 @@ Pluto allows us to interactively explore and experiment with our models. Here we
 begin
 	# Seed for random number generator for full graph
 	seed_smoothing_slider = @bind(
-		seed_smoothing, 
-		ThrottledSlider(1:100, default = 42, show_value = true)
+		seed_smoothing, ThrottledSlider(1:100, default = 42)
 	)
 	
 	# Number of observations on our model for full graph
 	n_smoothing_slider = @bind(
-		n_smoothing, 
-		ThrottledSlider(1:200, default = 100, show_value = true)
+		n_smoothing, ThrottledSlider(1:200, default = 100)
 	)
 	
 	# θ parameter is a rotation angle for transition matrix
 	θ_smoothing_slider = @bind(
-		θ_smoothing, 
-		ThrottledSlider(
-			range(0.0, π/2, length = 100), default = π/20, show_value = true
-		)
+		θ_smoothing, ThrottledSlider(range(0.0, π/2, length = 100), default = π/20)
 	)
 end;
 
@@ -202,7 +198,11 @@ begin
 	local x_reshaped = x_smoothing |> reshape_data
 	local y_reshaped = y_smoothing |> reshape_data
 	
-	local p = plot()
+	local p = plot(
+		title = "Smoothing synthetic dataset", titlefontsize = 10,
+		xlabel = "Time step k", xguidefontsize = 8,
+		ylabel = "Latent states values", yguidefonrsize = 8
+	)
 	local range = 1:n_smoothing
 	
 	p = plot!(p, range, x_reshaped, label = x_label)
@@ -229,24 +229,26 @@ function inference_full_graph(observations, A, B, P, Q)
 	# We call a `linear_gaussian_ssm_full_graph` function 
 	# from our model specification above
     model, (x, y) = linear_gaussian_ssm_full_graph(
-		n, A, B, P, Q, options = (limit_stack_depth = 100, )
+		n, A, B, P, Q, options = (limit_stack_depth = 500, )
 	)
     
 	# Rocket.jl provides some handy default actors
 	# `buffer` actor simply copies all received updates 
 	# into an internal buffer with length n
     xbuffer = buffer(Marginal, n)
+	bfe     = ScoreActor(Float64)
     
 	# For a collection of random variables we can use 
 	# `getmarginals()` function which returns a stream of vectors
     xsubscription = subscribe!(getmarginals(x), xbuffer)
+	fsubscription = subscribe!(score(Float64, BetheFreeEnergy(), model), bfe)
     
     update!(y, observations)
     
 	# Usually we need to unsubscribe every time we're done with our model
     unsubscribe!(xsubscription)
     
-    return getvalues(xbuffer)
+    return map(getvalues, (xbuffer, bfe))
 end
 
 # ╔═╡ 84c171fc-fd79-43f2-942f-7ec6acd63c14
@@ -261,7 +263,7 @@ md"""
 """
 
 # ╔═╡ 8981b0f8-9ff2-4c90-a958-0fe4da538809
-x_smoothing_estimated = inference_full_graph(
+x_smoothing_estimated, bfe_smoothing = inference_full_graph(
 	y_smoothing, 
 	A_smoothing, 
 	B_smoothing, 
@@ -285,7 +287,11 @@ begin
 	local x_inferred_means = mean.(x_smoothing_estimated) |> reshape_data
 	local x_inferred_stds  = var.(x_smoothing_estimated) |> reshape_data
 	
-	local p = plot()
+	local p = plot(
+		title = "Smoothing inference results", titlefontsize = 10,
+		xlabel = "Time step k", xguidefontsize = 8,
+		ylabel = "Latent states values", yguidefonrsize = 8
+	)
 	local range = 1:n_smoothing
 	
 	p = plot!(p, range, x_reshaped, label = x_label)
@@ -347,6 +353,7 @@ function inference_single_time_segment(observations, A, B, P, Q)
 	# We want to keep a full history of posterior marginal updates 
 	# for all time steps
     xbuffer = keep(Marginal)
+	bfe     = ScoreActor(Float64)
     
 	# Here we redirect our posterior marginal distribution from the current
 	# time step to the priors and proceed with the next time step
@@ -356,6 +363,7 @@ function inference_single_time_segment(observations, A, B, P, Q)
     end)
     
     xsubscription = subscribe!(getmarginal(x_t), xbuffer)
+	fsubscription = subscribe!(score(Float64, BetheFreeEnergy(), model), bfe)
     
 	# Priors for very first observation
     update!(x_min_t_mean, [ 0.0, 0.0 ])
@@ -363,12 +371,13 @@ function inference_single_time_segment(observations, A, B, P, Q)
     
     for observation in observations
         update!(y_t, observation)
+		release!(bfe)
     end
     
     unsubscribe!(xsubscription)
     unsubscribe!(redirect_to_prior)
     
-    return getvalues(xbuffer)
+    return map(getvalues, (xbuffer, bfe))
 end
 
 # ╔═╡ b8dea87a-258a-4849-bb12-7e9b5d1420ae
@@ -376,22 +385,17 @@ begin
 
 	# Seed for random number generator for filtering case
 	seed_filtering_slider = @bind(
-		seed_filtering, 
-		ThrottledSlider(1:100, default = 42, show_value = true)
+		seed_filtering, ThrottledSlider(1:100, default = 42)
 	)
 	
 	# Number of observations on our model for filtering case
 	n_filtering_slider = @bind(
-		n_filtering, 
-		ThrottledSlider(1:200, default = 100, show_value = true)
+		n_filtering, ThrottledSlider(1:200, default = 100)
 	)
 	
 	# θ parameter is a rotation angle for transition matrix
 	θ_filtering_slider = @bind(
-		θ_filtering, 
-		ThrottledSlider(
-			range(0.0, π/2, length = 100), default = π/20, show_value = true
-		)
+		θ_filtering, ThrottledSlider(range(0.0, π/2, length = 100), default = π/20)
 	)
 end;
 
@@ -437,7 +441,11 @@ begin
 	local x_reshaped = x_filtering |> reshape_data
 	local y_reshaped = y_filtering |> reshape_data
 	
-	local p = plot()
+	local p = plot(
+		title  = "Filtering synthetic dataset", titlefontsize = 10,
+		xlabel = "Time step k", xguidefontsize = 8,
+		ylabel = "Latent states values", yguidefonrsize = 8
+	)
 	local range = 1:n_filtering
 	
 	p = plot!(p, range, x_reshaped, label = x_label)
@@ -459,7 +467,7 @@ md"""
 """
 
 # ╔═╡ 989923c9-6871-449f-91eb-d20db563d568
-x_filtering_estimated = inference_single_time_segment(
+x_filtering_estimated, bfe_filtering = inference_single_time_segment(
 	y_filtering, 
 	A_filtering, 
 	B_filtering, 
@@ -483,7 +491,11 @@ begin
 	local x_inferred_means = mean.(x_filtering_estimated) |> reshape_data
 	local x_inferred_stds  = var.(x_filtering_estimated) |> reshape_data
 	
-	local p = plot()
+	local p = plot(
+		title = "Filtering inference results", titlefontsize = 10,
+		xlabel = "Time step k", xguidefontsize = 8,
+		ylabel = "Latent states values", yguidefonrsize = 8
+	)
 	local range = 1:n_filtering
 	
 	p = plot!(p, range, x_reshaped, label = x_label)
@@ -498,6 +510,137 @@ end
 md"""
 Compared to the previous demo (smoothing), the state estimation algorithm in this demo only passes messages forward in time. Therefore, the state estimates are less accurate than the smoothing result of the previous demo.
 """
+
+# ╔═╡ 4d3f9bdd-f89d-4fc4-924d-55008cd2c979
+md"""
+### Benchmarking
+
+In this section we will benchmark inference performance with the help of BenchmarkTools package. `ReactiveMP.jl` has been designed to be efficient and scalable as much as possible. To show `ReactiveMP.jl` performance capabilities we run a series of benchmark tests for linear gaussian state space model with different number of observations and different seeds and θ parameters. We show that execution time scales linearly on number of observations and does not depend on seed or θ. 
+"""
+
+# ╔═╡ 10965ed2-da40-4b6c-80a8-2f84590803a8
+function run_benchmark(inference_fn::Function, params)
+	@unpack n, seed, θ = params
+	
+	A = [ 
+		cos(θ) -sin(θ); 
+		sin(θ) cos(θ) 
+	]
+	B = [ 1.3 0.0; 0.0 0.7 ]
+	P = [ 1.0 0.0; 0.0 1.0 ]
+	Q = [ 1.0 0.0; 0.0 1.0 ]
+	
+	x, y = generate_data(
+		n    = n, 
+		A    = A, 
+		B    = B, 
+		P    = P, 
+		Q    = Q, 
+		seed = seed
+	)
+	
+	x_estimated, bfe = inference_full_graph(y, A, B, P, Q);
+	benchmark        = @benchmark $inference_fn($y, $A, $B, $P, $Q)
+	
+	output = @strdict n seed θ x_estimated bfe benchmark
+	
+	return output
+end
+
+# ╔═╡ fe122f6a-a30d-4fad-abf5-2c62cac09836
+# Here we create a list of parameters we want to run our benchmarks with
+benchmark_allparams = dict_list(Dict(
+	"n"    => [ 50, 100, 250, 500, 750, 1000, 1500, 2000, 2500, 5000, 10000 ],
+	"seed" => [ 42, 123 ],
+	"θ"    => [ π / 12, π / 24 ]
+));
+
+# ╔═╡ f8b90eeb-719f-456d-9fe5-d84fca13c65c
+md"""
+We want to perform benchmarking for different sets of parameters. In principal, timings should not depend on seed and θ parameters, but only on n.
+"""
+
+# ╔═╡ 425cccc7-6a38-477b-a128-7a513b056e4c
+# First run maybe slow, you may track the progress in the terminal
+# Subsequent runs will not create a new benchmarks 
+# but will reload it from data folder
+smoothing_benchmarks = map(benchmark_allparams) do params
+	path = datadir("benchmark", "lgssm", "smoothing")
+	result, _ = produce_or_load(path, params) do p
+		run_benchmark(inference_full_graph, p)
+	end
+	return result
+end;
+
+# ╔═╡ d38bc0a0-3fde-4d71-aa4c-11a83532fc05
+# First run maybe slow, you may track the progress in the terminal
+# Subsequent runs will not create a new benchmarks 
+# but will reload it from data folder
+filtering_benchmarks = map(benchmark_allparams) do params
+	path = datadir("benchmark", "lgssm", "filtering")
+	result, _ = produce_or_load(path, params) do p
+		run_benchmark(inference_single_time_segment, p)
+	end
+	return result
+end;
+
+# ╔═╡ 4ab06bc1-15b1-4d80-9b19-93fc09c1fbc6
+target_seed = 123
+
+# ╔═╡ a4abc2be-034b-44d9-b78d-2d889ebe0acb
+target_θ = π / 24
+
+# ╔═╡ 653dafb5-173d-40f7-93b3-ae4fbfb5d0d6
+md"""
+Here we plot benchmark timings for smoothing algorithm against number of observation. As we can see from benchmark results execution time scales linearly with the number of observations. Moreover we see that filtering as roughly twice faster as smoothing. This is in line with expectations because smoothing does both forward and backward passes, while filtering does onle forward.
+"""
+
+# ╔═╡ d485a985-39ec-422b-9476-d55b98786093
+begin
+	local s_filtered = filter(smoothing_benchmarks) do b
+		return b["θ"] === target_θ && b["seed"] === target_seed
+	end
+	
+	local f_filtered = filter(filtering_benchmarks) do b
+		return b["θ"] === target_θ && b["seed"] === target_seed
+	end
+	
+	@assert length(s_filtered) !== 0 "Empty benchmark set"
+	@assert length(f_filtered) !== 0 "Empty benchmark set"
+	
+	local s_range      = map(f -> f["n"], s_filtered)
+	local s_benchmarks = map(f -> f["benchmark"], s_filtered)
+	local s_timings    = map(t -> t.time, minimum.(s_benchmarks)) ./ 1_000_000
+	local s_memories   = map(t -> t.memory, minimum.(s_benchmarks)) ./ 1024
+	
+	local f_range      = map(f -> f["n"], f_filtered)
+	local f_benchmarks = map(f -> f["benchmark"], f_filtered)
+	local f_timings    = map(t -> t.time, minimum.(f_benchmarks)) ./ 1_000_000
+	local f_memories   = map(t -> t.memory, minimum.(f_benchmarks)) ./ 1024
+	
+	local p = plot(
+		title = "Linear Gaussian State Space Model Benchmark",
+		titlefontsize = 10, legend = :bottomright,
+		xlabel = "Number of observations in dataset (log-scale)", 
+		xguidefontsize = 9,
+		ylabel = "Time (in ms, log-scale)", 
+		yguidefontsize = 9
+	)
+	
+	p = plot!(
+		p, s_range, s_timings, 
+		yscale = :log10, xscale = :log10,
+		markershape = :utriangle, label = "Smoothing"
+	)
+	
+	p = plot!(
+		p, f_range, f_timings, 
+		yscale = :log10, xscale = :log10,
+		markershape = :diamond, label = "Filtering"
+	)
+	
+	@saveplot p "lgssm_smoothing_benchmark"
+end
 
 # ╔═╡ Cell order:
 # ╠═d160581c-9d1f-11eb-05f7-c5f29954488b
@@ -534,5 +677,15 @@ Compared to the previous demo (smoothing), the state estimation algorithm in thi
 # ╟─b006a807-016d-41aa-91ec-a02a4c270990
 # ╟─952cce56-c832-47cc-95ec-6c0d114add79
 # ╠═989923c9-6871-449f-91eb-d20db563d568
-# ╟─3beedf02-e870-4da9-89f4-a2667e5bee18
+# ╠═3beedf02-e870-4da9-89f4-a2667e5bee18
 # ╟─c082bbff-08ce-461e-a096-0df699a6f12d
+# ╠═4d3f9bdd-f89d-4fc4-924d-55008cd2c979
+# ╠═10965ed2-da40-4b6c-80a8-2f84590803a8
+# ╠═fe122f6a-a30d-4fad-abf5-2c62cac09836
+# ╟─f8b90eeb-719f-456d-9fe5-d84fca13c65c
+# ╠═425cccc7-6a38-477b-a128-7a513b056e4c
+# ╠═d38bc0a0-3fde-4d71-aa4c-11a83532fc05
+# ╠═4ab06bc1-15b1-4d80-9b19-93fc09c1fbc6
+# ╠═a4abc2be-034b-44d9-b78d-2d889ebe0acb
+# ╟─653dafb5-173d-40f7-93b3-ae4fbfb5d0d6
+# ╟─d485a985-39ec-422b-9476-d55b98786093
